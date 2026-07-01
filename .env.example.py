5f1b87860c3f63c62a3191fadd7c51c43fdfc5ea
Ари <arianna2811@yandex.ru>
2026-07-01 22:14:52 +0300

    feat: env.example for running program

---
+++ .env.example.py
@@ -0,0 +1,8 @@
+# here is example of private data
+
+sql_url = "postgresql://postgres:password@localhost:5432/postgres"
+
+secret_key_jwt = "VIBIRASHKA_SECRET_EXAMPLE"
+
+default_admin_email = "admin@vibirashka.ru"
+default_admin_password = "Admin12345"
